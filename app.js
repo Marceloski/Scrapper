73748e6bd6ed8c992656ab979541fcd2a79bb39f
Cristobal Caceres <cristobal.caceres.c@mail.pucv.cl>
2023-09-25 01:07:40 -0300

    ahora se es capaz de revisar si una subcategoria tiene mas subcategorias, si es asi, la subcategoria anterior se quita y se agregar las nuevas subcategorias

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer");
-const fs = require('fs');
+const fs = require("fs");
 
 async function App() {
   const browser = await puppeteer.launch({
@@ -27,8 +27,7 @@ async function App() {
 
         items.forEach((item) => {
           const a_element = item.querySelector("a");
-          hrefValue = a_element.getAttribute("href");
-          subcategories.push(hrefValue);
+          subcategories.push(a_element.getAttribute("href"));
         });
 
         categoriesCollection.push({
@@ -46,29 +45,61 @@ async function App() {
   //console.log(categories);
 
   for (const category of categories) {
-    for (const subcategory of category.subcategories) {
+    for (let i = category.subcategories.length - 1; i >= 0; i--) {
+      const subcategory = category.subcategories[i];
+      /* 
+        PARA RESOLVER PROBLEMA DE SUBCATEGORIAS EXTENDIDAS: 
+        1) Hacer goto a href de subcategory
+        2) Hacer evaluate a pagina de subcategory
+        3) Verificar si existe contenedor principal "paraSearch", si no existe, es porque es un producto.
+        4) Si existe un elemento tabla en contenedor principal, significa que no tiene mas subcategorias. href se deja tal cual.
+        5) Si no existe un elemento tabla en contenedor principal, significa que tiene mas subcategorias. Se debe reemplazar href 
+          de subcategoria actual con los href de las sub-subcategorias.
+      */
       await page.goto(subcategory);
-      //verificar si hay mas subcategorias
-      const msg = await page.evaluate((subcategory) => {
+      /*page.evaluate retornara un arreglo de href (nuevas subcategorias) 
+      si es que existen mas subcategorias. En caso contrario retornara null.*/
+      const moreSubCategories = await page.evaluate(() => {
         const mainContainer = document.getElementById("paraSearch");
         if (mainContainer) {
           const tableExists = mainContainer.querySelector("table");
           if (tableExists) {
-            return "Si tiene tabla "+subcategory;
+            return null;
           } else {
-            return "No tiene tabla "+subcategory;
+            const nav = mainContainer.querySelector("nav");
+            const items = nav.querySelectorAll("li");
+            const newSubCategories = [];
+
+            items.forEach((item) => {
+              const divContainer = item.querySelector(".productName");
+              const a_element = divContainer.querySelector("a");
+              newSubCategories.push(a_element.getAttribute("href"));
+            });
+
+            return newSubCategories;
           }
         } else {
-          return "no se encuentra paraSearch en" + subcategory;
+          //verificar si es un producto
+          return null;
         }
-      }, subcategory);
+      });
+
+      if (moreSubCategories) {
+        //eliminar href de subcategoria actual y agregar href de las sub-subcategorias.
+        category.subcategories.splice(i, 1);
+        category.subcategories.push(...moreSubCategories);
+        console.log(category);
+      }
       //generar txt con los links que no tienen tabla, para poder recorrerlos
       //console.log(msg);
-      fs.appendFileSync('linksTable.txt', msg + '\n');
+      //fs.appendFileSync('linksTable.txt', msg + '\n');
 
       await new Promise((r) => setTimeout(r, 5000)); // 10000 milisegundos = 10 segundos
     }
   }
+
+  console.log(categories);
+
   await browser.close();
 }
 
